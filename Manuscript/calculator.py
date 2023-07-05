c9417cde5f6fa23bd4a760580c194391754519f5
DeepthiNiharika <dchittineeraj1758@conestogac.on.ca>
2023-07-04 22:21:46 -0400

    lab 5

--- Manuscript/calculator.py
+++ Manuscript/calculator.py
@@ -1,16 +1,11 @@
 def add(num1, num2):
     return num1 + num2
 
-
 def subtract(num1, num2):
     return num1 - num2
 
-
 def multiply(num1, num2):
     return num1 * num2
-
-
-
 
 def divide(num1, num2):
     if num2 == 0:
